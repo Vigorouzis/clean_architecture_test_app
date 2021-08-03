12ea80e8a6e18770a940dd3402e99a70b6dc3299
Nikita <Nkanunov@gmail.com>
2021-08-03 13:40:27 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/clean_architecture_test_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clean_architecture_test_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:story_view/controller/story_controller.dart';
+import 'package:story_view/widgets/story_view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => StoryScreen(),
+            )),
+            child: Text('Open story'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class StoryScreen extends StatefulWidget {
+  const StoryScreen({Key? key}) : super(key: key);
+
+  @override
+  _StoryScreenState createState() => _StoryScreenState();
+}
+
+class _StoryScreenState extends State<StoryScreen> {
+  StoryController? _storyController;
+
+  List<StoryItem?> _storyItems = [
+    StoryItem.pageProviderImage(AssetImage('assets/start_video/11a.gif'),
+        imageFit: BoxFit.cover),
+    StoryItem.pageProviderImage(AssetImage('assets/start_video/22a.gif'),
+        imageFit: BoxFit.cover),
+    StoryItem.pageProviderImage(AssetImage('assets/start_video/33a.gif'),
+        imageFit: BoxFit.cover),
+    StoryItem.pageProviderImage(AssetImage('assets/start_video/44a.gif'),
+        imageFit: BoxFit.cover)
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _storyController = StoryController();
+  }
+
+  @override
+  void dispose() {
+    _storyController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          child: StoryView(
+            //  onComplete: () => Navigator.of(context).pop(),
+            controller: _storyController!,
+            storyItems: _storyItems,
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 39, left: 16, right: 16),
+            child: Row(
+              children: [
+                TextButton(
+                  onPressed: () => print('hello'),
+                  child: Text(
+                    'войти',
+                    style: TextStyle(fontFamily: 'Noah',fontWeight: FontWeight.w700, fontSize: 16, color: Colors.black),
+                  ),
+                ),
+                Spacer(),
+                Icon(
+                  Icons.arrow_forward,
+                  color: Colors.white,
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
